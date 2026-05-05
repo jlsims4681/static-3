8549727546b75a8700fb4334268403529cb426f8
jlsims4681 <jlsims2@students.wcpss.net>
2026-05-04 22:49:57 -0400

    Update sw.js

--- active/sw.js
+++ active/sw.js
@@ -4,6 +4,14 @@ importScripts(__uv$config.sw || '/uv/uv.sw.js');
 
 const sw = new UVServiceWorker();
 
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('fetch', (event) => {
     event.respondWith(sw.fetch(event));
 });
