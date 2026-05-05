0e0e48c1871d79dabc0140dc0e49c2b9eeefbf3e
jlsims4681 <jlsims2@students.wcpss.net>
2026-05-05 17:30:52 -0400

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "https://tomp.app/", 
+  bare: "https://bare.benrogo.net/", 
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
