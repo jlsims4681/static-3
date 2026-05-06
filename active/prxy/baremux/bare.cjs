980efaeac7bd9553b831d4910460bd0940798752
jlsims4681 <jlsims2@students.wcpss.net>
2026-05-06 19:47:23 -0400

    Update bare.cjs

--- active/prxy/baremux/bare.cjs
+++ active/prxy/baremux/bare.cjs
@@ -1,2 +1,7 @@
-/* BareMux Transport Script */
-const Bare=JSON.parse(JSON.stringify({}));async function setup(){return{ready:!0}}Bare.init=setup;Bare.ready=setup;Bare.connect=async(e,t,r)=>{const n=await fetch(e,{method:"GET",headers:t,redirect:"follow"});return n};export default Bare;
+'use strict';
+
+const { resolve } = require('node:path');
+
+const baremuxPath = resolve(__dirname, '..', 'dist');
+
+exports.baremuxPath = baremuxPath;
