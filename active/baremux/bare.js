3834b4d4760d2aab566451cd3db62c9cd747abab
jlsims4681 <jlsims2@students.wcpss.net>
2026-05-11 20:10:38 -0400

    Create bare.js

---
+++ active/baremux/bare.js
@@ -0,0 +1,4 @@
+<script src="https://unpkg.com/@mercuryworkshop/bare-transport@2/dist/index.js"></script>
+<script>
+  let client = new BareMod.BareClient("https://mybareserv.onrender.com/");
+</script>
